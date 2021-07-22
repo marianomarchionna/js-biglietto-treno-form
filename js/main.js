31f60a6ef2b9bedd0447f620be1e162c07b1a6b2
Mariano <marianomarchionna8@gmail.com>
2021-07-22 17:06:05 +0200

    Section biglietto

--- js/main.js
+++ js/main.js
@@ -1,8 +1,23 @@
-var costo = num_km * 0.21;
+// var btnGenera = document.getElementById('genera');
+// btnGenera.addEventListener('click',
+//     function() {
+//         var nome = document.getElementById('nome').value;
+//         var km = document.getElementById('km').value;
+//         var eta = document.getElementById('fascia-eta').value;
+//     }
+// )
 
-if (eta < 18) {
-    costo = costo * 0.8;
-} else if (eta >= 65) {
-    costo = costo * 0.6;
-} 
-costo = costo.toFixed(2);
+// var costo = km * 0.21;
+
+// if (eta == minorenne) {
+//     costo = costo * 0.8;
+// } else if (eta == sover65) {
+//     costo = costo * 0.6;
+// } 
+// costo = costo.toFixed(2);
+
+var numCarrozza = Math.floor(Math.random() * 15) + 1;
+document.getElementById('numero-carrozza').innerHTML = numCarrozza;
+
+var codTreno = Math.floor(Math.random() * 9999) + 90000;
+document.getElementById('codice-treno').innerHTML = codTreno;
